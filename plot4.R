828820d57a2fc82377d1d8dde778d1fabe4bfab1
Ruifeng liu <hwinchem@gmail.com>
2014-07-10 16:12:08 -0500

    plot1

--- plot4.R
+++ plot4.R
@@ -32,7 +32,7 @@ legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"
          lty=c(1,1,1), col=c("black","red","blue"), bty="n")
 
 
-plot(data$datetime, data$Voltage<-seq(234, 246, by=4), ylab="Voltage", xlab="datetime", type="l")
+plot(data$datetime, data$Voltage, ylab="Voltage", xlab="datetime", type="l")
 
 
 plot(data$datetime, data$Global_reactive_power, ylab="Global_reactive_power",
